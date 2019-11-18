69271c49d3b042c682aa8cf1a1c43e9539232732
Makoto Yui <myui@apache.org>
2019-11-18 17:54:49 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+# Xxx4j
+[![Build Status](https://travis-ci.org/myui/xxx4j.svg?branch=master)](https://travis-ci.org/myui/xxx4j)
+[![License](http://img.shields.io/:license-Apache_v2-blue.svg)](https://github.com/myui/xxx4j/blob/master/LICENSE)
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/io.github.myui/xxx4j/badge.svg)](https://maven-badges.herokuapp.com/maven-central/io.github.myui/xxx4j)
+
+The ????? library for Java
---
+++ scaffold.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright 2019 and onwards Makoto Yui
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+echo "Please input project name (e.g., matrix4j)"
+echo -n ">> "
+read proj_name
+echo
+
+artifact_name=`echo ${proj_name} | awk '{print tolower($0)}'`
+proj_title=`echo ${proj_name} | awk '{print toupper(substr($0,0,1))substr($0,2)}'`
+
+echo "artifact_name=${artifact_name}"
+echo "proj_title=${proj_title}"
+
+find . -type f | xargs grep -ie "${proj_name}"
+
+# find . -type f | xargs sed -i "" "s/xxx4j/${artifact_name}"
+# find . -type f | xargs sed -i "" "s/Xxx4j/${proj_title}"
